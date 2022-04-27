0d0235f25372b36aa9994b0405b63b4d053bce8f
kymhd <mahabamema15@gmail.com>
2022-04-27 05:44:22 +0000

    amelioration du jeu

--- python/Jeux_De_Math.py
+++ python/Jeux_De_Math.py
@@ -6,11 +6,60 @@ ceci est un programme de debutant, ça m'a permis d'apprendre les bases du langa
 
 """
 import random
+nbr_max = 0
+nbr_min = 0
 
-nbr_min = 2
-nbr_max = 5
+liste = [
+    "Débutant",
+    "Intermediaire",
+    "Gourou"
+]
+print()
 
-NOMBRE_DE_QUESTION = 4
+print("BIENVENUE DANS LE JEU DE MATH")
+print()
+
+print("Quel est votre niveau parmis les 3 ?")
+count = 1
+for i in liste:
+    print(f"{count} ==> {i} ")
+    count += 1
+
+print()
+
+def niveau():
+    global nbr_min
+    global nbr_max
+    niveau = 0
+    while niveau <= 0 or niveau > 3:
+        niveau = input("mettez votre niveau, 1, 2 ou 3: ")
+        try:
+            niveau = int(niveau)
+        except ValueError:
+            print("Vous n'avez pas entrez un nombre")
+            niveau = -1
+            continue
+        print()
+        if niveau > 3:
+            print(f"{niveau} ne fait pas partie ! choisisez entre 1, 2, 3")
+            print()
+        if niveau <= 0:
+            print("choisisez entre 1, 2, 3")
+
+    if niveau == 1:
+        nbr_min = 2
+        nbr_max = 7
+
+    elif niveau == 2:
+        nbr_min = 2
+        nbr_max = 16
+
+    else:
+        nbr_min = 2
+        nbr_max = random.randint(9,60)
+
+    return nbr_min, nbr_max
+
 
 def poser_question():
     a = random.randint(nbr_min, nbr_max)
@@ -45,6 +94,26 @@ def poser_question():
 
     return False
 
+def demander_nbr_question():
+    nbre = 0
+    while nbre <= 0:
+        nbre = input("Entrez le nombre de question que vous souhaitez jouer: ")
+        try:
+            nbre = int(nbre)
+        except ValueError:
+            print("Vous n'avez pas entre un chiffre")
+            nbre = -1
+            continue
+        if nbre < 0:
+            print("Vous avez entrez un nombre negatif")
+        if nbre == 0:
+            print("ERREUR, Vous avez entrez 0")
+    return nbre
+
+nombre_max, nombre_min = niveau()
+NOMBRE_DE_QUESTION = demander_nbr_question()
+
+print()
 note = 0
 for i in range(0, NOMBRE_DE_QUESTION): #la boucle quicontrole le nombre des questions, et qui verifie les reponses
     print(f"La question Numero {i+1} sur {NOMBRE_DE_QUESTION}")
@@ -58,7 +127,7 @@ for i in range(0, NOMBRE_DE_QUESTION): #la boucle quicontrole le nombre des ques
 print(f"Votre note Finale est de : {note} / {NOMBRE_DE_QUESTION}") #affichage du note obtenue par l"user
 
 """
-verification si l'utilisateur a eu la moyenne ou pas, et aussi, s'il a trouvé toutes les réponses !
+verification si l'utilisateur a eu la moyenne ou pas !
 """
 
 if note == NOMBRE_DE_QUESTION:
